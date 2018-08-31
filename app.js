c18a7ee508159341fecf8db05fb399d05ee1b958
STH <459904593@qq.com>
2018-08-31 22:13:00 +0800

    add two project

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
-/*var Players = require('./lib/Players.js');
-var Player = require('./lib/Player.js');*/
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var mysql = require('mysql');
+var pool = mysql.createPool({
+    connectionLimit: 10,
+    host: 'localhost',
+    user: '',
+    password: '',
+    port: '3306',
+    database: 'say',
+});
 
 app.use(express.static('public'));
 
@@ -11,6 +18,15 @@ app.get('/', function(req, res) {
     res.sendfile('page/index.html');
 });
 
+//chat
+app.get('/chat', function(req, res) {
+    res.sendfile('page/chat.html');
+});
+require('./lib/chat/main.js').chat(app,io.of('/chat'),pool);
+//zzu
+app.get('/zzu', function(req, res) {
+    res.sendfile('page/zzu.html');
+});
 //1010
 app.get('/1010', function(req, res) {
     res.sendfile('page/1010.html');
